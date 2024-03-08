f3fd31eb5a10bce468298421e060840a074ee612
Nicolas Lemoine <nico.lemoine@gmail.com>
2024-03-08 13:19:30 +0100

    feat!: release 2.0
    - replace hellonico/twig-dump-extension with  symfony/twig-bridge
    - use wecodemore/wordpress-early-hook to ensure hook is added event if autoloaded early

--- functions.php
+++ functions.php
@@ -2,14 +2,16 @@
 
 namespace HelloNico\Timber;
 
-use HelloNico\Twig\DumpExtension;
+use Symfony\Bridge\Twig\Extension\DumpExtension;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+
+use function WeCodeMore\earlyAddFilter;
 
 function add_dump_extension($twig)
 {
-    $twig->addExtension(new DumpExtension());
+    if (defined('WP_DEBUG') && WP_DEBUG) {
+        $twig->addExtension(new DumpExtension(new VarCloner()));
+    }
     return $twig;
 }
-
-if (defined('WP_DEBUG') && WP_DEBUG && function_exists('add_filter')) {
-    add_filter('timber/loader/twig', sprintf('%s\\add_dump_extension', __NAMESPACE__));
-}
+earlyAddFilter('timber/loader/twig', sprintf('%s\\add_dump_extension', __NAMESPACE__));
